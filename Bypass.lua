204422b01e23321f46539bd8c97a0d9bd54ef692
Fsploit <169143383+Fsploit@users.noreply.github.com>
2025-04-22 09:23:01 +0800

    Update Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -1,13 +1,7 @@
-local free_users_access = true
+--[[
 
---[[ 
-    [⚠️] - Take Note
-    Please note that by using this script, you are allowing us to view your Roblox account profile as in 
-    https://www.roblox.com/users/1/profile. This is to take precautions on who is using our script 
-    and blacklist those attempting to modify it.
-  
-    [🔴] - Issues
-    If you have any issues with our hub or encounter problems, please contact us at https://discord.gg/GetFrost.
-]]
+ZEPHYR 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Fsploit/Loader/refs/heads/main/loaders-v3"))("Frostware on top! dsc.gg/GetFrost")
+]]--
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/438e15470ef08d45017e8094075f3f40.lua"))()
